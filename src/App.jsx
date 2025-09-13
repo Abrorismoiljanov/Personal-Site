63047b0e1667a1fb9d06608dc2931225913b885c
Abror <Krubka_Jo@protonmail.com>
2025-09-13 23:01:22 +0500

    Video path corrected

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-     <video className="background" src="/synthwave-aesthetics-moewalls-com.mp4" autoPlay loop playsInline mute ></video>
+     <video className="background" src="/synthwave-aesthetics-moewalls-com.mp4" autoPlay loop playsInline muted ></video>
      <div className="layout">
       <Navigation setCurrentPage={setCurrentPage}/>
      <ContentHolder Load={pages[currentPage]}/>
