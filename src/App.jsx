ecaed0f5e8b705eaf710f7668828f07ab226efc3
Abror <Krubka_Jo@protonmail.com>
2025-09-13 23:14:46 +0500

    Video path corrected

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-     <video className="background" src="/assets/background.mp4" autoPlay loop playsInline muted ></video>
+     <video className="background" src="background.mp4" autoPlay loop playsInline muted ></video>
      <div className="layout">
       <Navigation setCurrentPage={setCurrentPage}/>
      <ContentHolder Load={pages[currentPage]}/>
